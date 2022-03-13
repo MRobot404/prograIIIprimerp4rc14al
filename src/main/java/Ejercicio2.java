bae4af6c598912f38098616d984366d57eb84c6b
Oscar Escobar <qomc16@gmail.com>
2022-03-13 14:47:14 -0600

    Ejercicio #2 finalizado

---
+++ src/main/java/Ejercicio2.java
@@ -0,0 +1,59 @@
+import rx.Observable;
+import rx.functions.Func2;
+
+import java.util.ArrayList;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        ArrayList<Consola> consolas = new ArrayList<>();
+        consolas.add(new Consola("ps4", 300));
+        consolas.add(new Consola("gamecube", 300));
+        consolas.add(new Consola("external disk", 200));
+        consolas.add(new Consola("laptop", 800));
+        consolas.add(new Consola("vr", 230));
+
+        Observable observableconsola
+                = Observable
+                .from(consolas.toArray())
+                .map((result) -> {
+                    Consola consola = (Consola) result;
+                    return consola.getPrecio();
+                })
+                .reduce(
+                        new Func2<Integer, Integer, Integer>() {
+                            @Override
+                            public Integer call(Integer acumulador, Integer actual) {
+                                return acumulador + actual;
+                            }
+                        }
+                );
+        observableconsola.subscribe((sumatoria) -> {
+            System.out.println("Sumatoria de los items: " + sumatoria + "\n");
+
+        });
+        Observable observableconsola2
+                = Observable
+                .from(consolas.toArray())
+                .map((result) ->{
+                    Consola consola = (Consola) result;
+                    return consola.getPrecio();
+                })
+                .reduce(
+                        new Func2<Integer, Integer, Integer>() {
+                            @Override
+                            public Integer call(Integer anterior, Integer actual) {
+                              if (actual <= anterior){
+
+                              }else{
+                                  anterior=actual;
+                              }
+                              return anterior;
+                            }
+                        }
+                );
+        observableconsola2.subscribe((verificador) ->{
+            System.out.println("Precio máximo del listado de producto: "+verificador);
+        });
+
+    }
+}
